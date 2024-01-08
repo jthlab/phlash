625d49620e3c8f8bac4ace5db41188ebc56d0698
Jonathan Terhorst <terhorst@gmail.com>
2024-01-08 10:17:34 -0500

    limit cache size

--- src/eastbay/memory.py
+++ src/eastbay/memory.py
@@ -1,4 +1,7 @@
 import appdirs
 from joblib import Memory
 
-memory = Memory(appdirs.user_cache_dir(__name__.split(".")[0]), verbose=0)
+memory = Memory(
+    appdirs.user_cache_dir(__name__.split(".")[0]), verbose=0, bytes_limit="5G"
+)
+memory.reduce_size()
