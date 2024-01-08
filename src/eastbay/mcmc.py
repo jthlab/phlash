28dbd6b85de0f32069585012803554d82e78eedc
Jonathan Terhorst <terhorst@gmail.com>
2024-01-08 10:43:15 -0500

    refactor model

--- src/eastbay/mcmc.py
+++ src/eastbay/mcmc.py
@@ -9,74 +9,16 @@ from jax import grad, jit
 from jax import numpy as jnp
 from jax import vmap
 from jax.flatten_util import ravel_pytree
-from jaxtyping import Array, Float64, Int8, Int64
 
 import eastbay.hmm
 import eastbay.liveplot
 from eastbay.gpu import PSMCKernel
 from eastbay.log import getLogger
-from eastbay.params import MCMCParams, PSMCParams
+from eastbay.model import log_density
+from eastbay.params import MCMCParams
 from eastbay.util import tree_unstack
 
 logger = getLogger(__name__)
-
-
-# prior and likelihood
-def _log_prior(mcp: MCMCParams) -> float:
-    dm = mcp.to_dm()
-    ret = sum(
-        jax.scipy.stats.norm.logpdf(a, loc=mu, scale=sigma).sum()
-        for (a, mu, sigma) in [
-            # (jnp.log(dm.eta.c), 0.0, 3.0),
-            (mcp.log_rho, jnp.log(dm.theta), 1.0),
-        ]
-    )
-    ret -= mcp.alpha * jnp.sum(jnp.diff(mcp.c_tr) ** 2)
-    return ret
-
-
-def _log_density(
-    mcp: MCMCParams,
-    c: Float64[Array, "3"],
-    inds: Int64[Array, "batch"],
-    warmup: Int8[Array, "c ell"],
-    kern: PSMCKernel,
-    afs: Int64[Array, "n"],
-) -> float:
-    r"""
-    Computes the log density of a statistical model by combining the contributions from
-    the prior, the hidden Markov model (HMM), and the allele frequency spectrum (AFS)
-    model, weighted by given coefficients.
-
-    Args:
-        mcp: The Markov Chain Monte Carlo parameters used to specify the model.
-        c: Weights for each component of the density - prior, HMM model, and AFS model.
-        inds: Mini-batch indices for selecting subsets of the data.
-        data: Data matrix used in the model computation.
-        kern: An instantiated PSMC Kernel used in the computation.
-        afs: The allele frequency spectrum data.
-
-    Returns:
-        The log density, or negative infinity where the result is not finite.
-    """
-    dm = mcp.to_dm()
-    pp = PSMCParams.from_dm(dm)
-    pis = vmap(lambda pp, d: eastbay.hmm.psmc_ll(pp, d)[0], (None, 0))(
-        pp, warmup
-    )  # (I, M)
-    pps = vmap(lambda pi: pp._replace(pi=pi))(pis)
-    l1 = _log_prior(mcp)
-    l2 = vmap(kern.loglik, (0, 0))(pps, inds).sum()
-    if afs is not None:
-        n = len(afs) + 1
-        etbl = dm.eta.etbl(n)
-        esfs = etbl / etbl.sum()
-        l3 = jax.scipy.special.xlogy(afs, esfs).sum()
-    else:
-        l3 = 0.0
-    ll = jnp.array([l1, l2, l3])
-    ret = jnp.dot(c, ll)
-    return jnp.where(jnp.isfinite(ret), ret, -jnp.inf)
 
 
 def _chunk_het_matrix(
@@ -193,7 +135,7 @@ def fit(
         )
     assert isinstance(init, MCMCParams)
     opt = optax.amsgrad(learning_rate=options.get("learning_rate", 0.1))
-    svgd = blackjax.svgd(grad(_log_density), opt)
+    svgd = blackjax.svgd(grad(log_density), opt)
 
     # set up the particles and add noise
     M = init.M
@@ -240,7 +182,7 @@ def fit(
         def elpd(mcps):
             @vmap
             def ll(mcp):
-                return _log_density(
+                return log_density(
                     mcp,
                     c=jnp.array([0.0, 1.0, 1.0]),
                     inds=jnp.arange(N_test),
---
+++ src/eastbay/model.py
@@ -0,0 +1,65 @@
+import jax
+import jax.numpy as jnp
+from jax import vmap
+from jaxtyping import Array, Float64, Int8, Int64
+
+import eastbay.hmm
+from eastbay.gpu import PSMCKernel
+from eastbay.params import MCMCParams, PSMCParams
+
+
+def log_prior(mcp: MCMCParams) -> float:
+    dm = mcp.to_dm()
+    ret = sum(
+        jax.scipy.stats.norm.logpdf(a, loc=mu, scale=sigma).sum()
+        for (a, mu, sigma) in [
+            # (jnp.log(dm.eta.c), 0.0, 3.0),
+            (mcp.log_rho, jnp.log(dm.theta), 1.0),
+        ]
+    )
+    ret -= mcp.alpha * jnp.sum(jnp.diff(mcp.c_tr) ** 2)
+    return ret
+
+
+def log_density(
+    mcp: MCMCParams,
+    c: Float64[Array, "3"],
+    inds: Int64[Array, "batch"],
+    warmup: Int8[Array, "c ell"],
+    kern: PSMCKernel,
+    afs: Int64[Array, "n"],
+) -> float:
+    r"""
+    Computes the log density of a statistical model by combining the contributions from
+    the prior, the hidden Markov model (HMM), and the allele frequency spectrum (AFS)
+    model, weighted by given coefficients.
+
+    Args:
+        mcp: The Markov Chain Monte Carlo parameters used to specify the model.
+        c: Weights for each component of the density - prior, HMM model, and AFS model.
+        inds: Mini-batch indices for selecting subsets of the data.
+        data: Data matrix used in the model computation.
+        kern: An instantiated PSMC Kernel used in the computation.
+        afs: The allele frequency spectrum data.
+
+    Returns:
+        The log density, or negative infinity where the result is not finite.
+    """
+    dm = mcp.to_dm()
+    pp = PSMCParams.from_dm(dm)
+    pis = vmap(lambda pp, d: eastbay.hmm.psmc_ll(pp, d)[0], (None, 0))(
+        pp, warmup
+    )  # (I, M)
+    pps = vmap(lambda pi: pp._replace(pi=pi))(pis)
+    l1 = log_prior(mcp)
+    l2 = vmap(kern.loglik, (0, 0))(pps, inds).sum()
+    if afs is not None:
+        n = len(afs) + 1
+        etbl = dm.eta.etbl(n)
+        esfs = etbl / etbl.sum()
+        l3 = jax.scipy.special.xlogy(afs, esfs).sum()
+    else:
+        l3 = 0.0
+    ll = jnp.array([l1, l2, l3])
+    ret = jnp.dot(c, ll)
+    return jnp.where(jnp.isfinite(ret), ret, -jnp.inf)
