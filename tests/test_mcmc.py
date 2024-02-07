a16e7d962043e6a7a78e80ca542de3b151fabd26
Jonathan Terhorst <terhorst@gmail.com>
2024-02-07 11:27:42 -0500

    fix mcmc test

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -1,17 +1,11 @@
-import numpy as np
-
-import phlash.data
 import phlash.mcmc
-
-
-def test_sp(rng):
-    x = rng.normal(size=100)
-    y = _squareplus(x)
-    np.testing.assert_allclose(x, _squareplus_inv(y))
+import phlash.sim
+from phlash.size_history import DemographicModel
 
 
 def test_functional1():
-    truth, chd = phlash.data.stdpopsim_dataset(
-        "SouthMiddleAtlas_1D17", "SouthMiddleAtlas"
-    )
-    phlash.mcmc.fit(chd, options=dict(niter=5))
+    truth, chd = phlash.sim.stdpopsim_dataset("HomSap", "Zigzag_1S14", {"generic": 20})
+    res = phlash.mcmc.fit(chd, options=dict(niter=5))
+    assert isinstance(res, list)
+    assert len(res) == 500
+    assert isinstance(res[0], DemographicModel)
