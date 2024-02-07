9bc15ad8f2b5b478b3e55fc7721cfe63d0f0a753
Jonathan Terhorst <terhorst@gmail.com>
2024-02-07 12:06:17 -0500

    make jaxgpu variant optional

--- src/phlash/__init__.py
+++ src/phlash/__init__.py
@@ -7,11 +7,19 @@ os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 import sys
 
 import jax
+from loguru import logger
 
 from phlash.data import contig
 from phlash.mcmc import fit
 
 jax.config.update("jax_enable_x64", True)
+if jax.local_devices()[0].platform != "gpu":
+    logger.warning(
+        "Detected that Jax is not running on GPU; you appear to have "
+        "CPU-mode Jax installed. Performance may be improved by installing "
+        "Jax-GPU instead. For installation instructions see:\n\n\t{}\n",
+        "https://github.com/google/jax?tab=readme-ov-file#installation",
+    )
 
 __all__ = ["fit", "contig"]
 
