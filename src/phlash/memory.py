1d6e5fb40393706f6c41d0482abbb701fee5da1c
Jonathan Terhorst <terhorst@gmail.com>
2024-02-08 10:47:07 -0500

    eliminate joblib.Memory deprecation warning

--- src/phlash/memory.py
+++ src/phlash/memory.py
@@ -1,7 +1,5 @@
 import appdirs
 from joblib import Memory
 
-memory = Memory(
-    appdirs.user_cache_dir(__name__.split(".")[0]), verbose=0, bytes_limit="5G"
-)
-memory.reduce_size()
+memory = Memory(appdirs.user_cache_dir(__name__.split(".")[0]), verbose=0)
+memory.reduce_size(bytes_limit="5G")
