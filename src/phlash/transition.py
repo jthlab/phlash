fix rho scaling constant

src/phlash/transition.py
@@ -43,7 +43,7 @@ def transition_matrix(dm: DemographicModel, n: int = 2) -> jax.Array:
     dt0 = jnp.isclose(dt_aug, 0.0)
     dt_safe = jnp.where(dt0, 1.0, dt_aug)
     cr = jnp.repeat(dm.eta.c, 2, axis=0)[:-1]
-    P = jax.vmap(_expQ, (0, 0, None))(dt_safe * dm.rho, dt_aug * cr, n)
+    P = jax.vmap(_expQ, (0, 0, None))(2 * dt_safe * dm.rho, dt_aug * cr, n)
     P = jnp.where(dt0[:, None, None], jnp.eye(3)[None], P)
     Pinf = jnp.array([[0.0, 0.0, 1.0]] * 3)
     P = jnp.concatenate([jnp.eye(3)[None], P, Pinf[None]], 0)