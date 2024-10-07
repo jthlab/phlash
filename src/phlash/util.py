1e1e214dc004b4ff48991df029297688c3a8fedb
Jonathan Terhorst <terhorst@gmail.com>
2024-10-07 09:18:07 -0400

    move plot code to utils

--- src/phlash/util.py
+++ src/phlash/util.py
@@ -1,8 +1,12 @@
 import jax
 import jax.numpy as jnp
 import jax.tree_util
+import matplotlib
 import numpy as np
+import scienceplots  # noqa: F401
 from scipy.interpolate import PPoly
+
+import phlash.size_history
 
 
 class Pattern:
@@ -58,3 +62,44 @@ def invert_cpwli(R: PPoly):
     b, a = R.c
     # the inverse func interpolates (R(x[i]), x[i]) for i = 0, 1, ..., n - 1
     return PPoly(x=np.append(a, np.inf), c=np.array([1 / b, x[:-1]]), extrapolate=False)
+
+
+def plot_posterior(
+    dms: list["phlash.size_history.DemographicModel"],
+    ax: "matplotlib.axes.Axes" = None,
+    credible_width: float = 0.95,
+    **kwargs,
+) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    """Plot the posterior distribution of the effective population size through time.
+
+    Args:
+        dms: list of DemographicModel objects
+        ax: matplotlib axes object
+        credible_width: credible interval width. If None, do not plot credible bands.
+        **kwargs: keyword arguments to pass to matplotlib.pyplot.plot
+
+    Returns:
+        t: time points
+        median: posterior median
+        credible_interval: credible interval (None if credible_bands is None)
+    """
+    if ax is None:
+        import matplotlib.pyplot as plt
+
+        plt.style.use("science")
+        ax = plt.gca()
+    dms = tree_stack(dms)
+    t1, tM = jnp.quantile(dms.eta.t[:, 1:], jnp.array([0.025, 0.975]))
+    t = jnp.geomspace(t1, tM, 1000)
+    Ne = jax.vmap(phlash.size_history.SizeHistory.__call__, (0, None, None))(
+        dms.eta, t, True
+    )
+    m = jnp.median(Ne, axis=0)
+    ax.plot(t, m, **kwargs)
+    if credible_width is not None:
+        alpha = (1 - credible_width) / 2
+        q_lb, q_ub = jnp.quantile(Ne, jnp.array([alpha, 1 - alpha]), axis=0)
+        ax.fill_between(t, q_lb, q_ub, alpha=0.1)
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    return t, m, jnp.array([q_lb, q_ub]) if credible_width is not None else None
