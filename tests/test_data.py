dafb4dab2e4f23a67eb46c6c350570cce476796a
Jonathan Terhorst <terhorst@gmail.com>
2024-11-04 15:59:08 -0500

    add multipop sfs

--- tests/test_data.py
+++ tests/test_data.py
@@ -33,9 +33,9 @@ def test_chunk(rng):
     overlap = 123
     chunk_size = 4_567
     ch = _chunk_het_matrix(H, overlap=overlap, chunk_size=chunk_size)
-    assert ch.shape == (3, overlap + chunk_size, 2)
+    assert ch.shape == (1, 3, overlap + chunk_size, 2)
     b = 0
-    for ch_i in ch:
+    for ch_i in ch[0]:
         q = min(chunk_size + overlap, len(H[0, b:]))
         assert np.all(ch_i[:q] == H[0, b : b + q])
         b += chunk_size
@@ -44,9 +44,11 @@ def test_chunk(rng):
 def test_psmcfa(psmcfa_file):
     rc = Contig.from_psmcfa(psmcfa_file, "1", 100)
     assert rc.window_size == 100
-    assert rc.hets.shape == (1, 100, 2)
-    assert rc.hets[0, :, 0].sum() == 100 * 99  # 1 missing entry has been inserted
-    assert rc.hets[0, :, 1].sum() == 81
+    assert rc.hets[0].shape == (1, 100, 2)
+    assert rc.hets[0][0, :, 0].sum() == 100 * 99  # 1 missing entry has been inserted
+    assert rc.hets[0][0, :, 1].sum() == 81
+    assert len(rc.populations) == 1
+    assert (rc.hets[1] == rc.populations[0]).all()
 
 
 def test_vcf():
@@ -57,10 +59,11 @@ def test_vcf():
         interval=(25_000_000, 26_000_000),
         sample_ids=["NA12878", "NA12889", ("NA12878", "NA12889")],
     )
-    assert vcfc.hets[..., 1].max() == 2
-    assert vcfc.hets[..., 1].sum() == 380
-    assert vcfc.afs.keys() == {4}  # sample size
-    assert np.all(vcfc.afs[4] == [143, 60, 89])
+    assert vcfc.hets[0][..., 1].max() == 2
+    assert vcfc.hets[0][..., 1].sum() == 380
+    assert vcfc.afs.keys() == {(4,)}  # sample size
+    assert vcfc.populations == (0,)
+    assert np.all(vcfc.afs[(4,)][1:-1] == [143, 60, 89])
 
 
 def test_vcf_ratemap():
@@ -70,11 +73,22 @@ def test_vcf_ratemap():
         .get_genetic_map("HapMapII_GRCh37")
         .get_chromosome_map("1")
     )
+    # not enough samples
+    with pytest.raises(ValueError):
+        Contig.from_vcf(
+            vcf_path=fn,
+            contig="1",
+            interval=(25_000_000, 26_000_000),
+            sample_ids=["NA12878", "NA12889"],
+            genetic_map=rm,
+            ld_buckets=np.arange(0.1, 0.4, 0.1),
+        )
+    fn = os.path.join(os.path.dirname(__file__), "fixtures", "sample_big.bcf")
     Contig.from_vcf(
         vcf_path=fn,
         contig="1",
-        interval=(25_000_000, 26_000_000),
-        sample_ids=["NA12878", "NA12889"],
+        interval=(20_000_000, 24_000_000),
+        sample_ids=[f"tsk_{i}" for i in range(4)],
         genetic_map=rm,
         ld_buckets=np.arange(0.1, 0.4, 0.1),
     )
@@ -94,27 +108,28 @@ def test_vcf_empty_samples():
 
 def test_ts(sim):
     tsc = Contig.from_ts(ts=sim, nodes=[(0, 1), (2, 3)])
-    assert tsc.hets[..., 1].max() == 3
-    assert tsc.hets[..., 1].sum() == 570
-    assert tsc.afs.keys() == {4}
-    assert np.all(tsc.afs[4] == [507, 172, 63])
+    assert tsc.hets[0][..., 1].max() == 3
+    assert tsc.hets[0][..., 1].sum() == 570
+    assert (tsc.hets[1] == 0).all()
+    assert tsc.afs.keys() == {(4,)}
+    assert np.all(tsc.afs[4,] == [0, 507, 172, 63, 0])
     assert np.all(
-        tsc.afs[4]
-        == sim.allele_frequency_spectrum(span_normalise=False, polarised=True)[1:-1]
+        tsc.afs[4,]
+        == sim.allele_frequency_spectrum(span_normalise=False, polarised=True)
     )
 
 
 def test_ts_repeated_nodes(sim):
     tsc = Contig.from_ts(ts=sim, nodes=[(0, 1), (1, 2), (0, 0)])
-    assert tsc.hets[..., 1].max() == 2
-    assert tsc.hets[..., 1].sum() == 591
-    assert tsc.afs.keys() == {6}
-    assert np.all(tsc.afs[6] == [290, 26, 14, 0, 235])
+    assert tsc.hets[0][..., 1].max() == 2
+    assert tsc.hets[0][..., 1].sum() == 591
+    assert tsc.afs.keys() == {(3,)}
+    assert np.all(tsc.afs[3,] == [0, 330, 235, 0])
 
 
 def test_ts_mask(sim):
     tsc = Contig.from_ts(ts=sim, nodes=[(0, 1), (2, 3)], mask=[(250, 1000)])
-    assert np.all(tsc.hets[:, 2:10, 0] == ([50] + [0] * 7))
+    assert np.all(tsc.hets[0][:, 2:10, 0] == ([50] + [0] * 7))
 
 
 def test_equal_ts_vcf(sim):
@@ -135,15 +150,46 @@ def test_equal_ts_vcf(sim):
             contig="1",
             interval=(1, 1_000_001),
         )
-    np.testing.assert_allclose(tsc.hets, vcfc.hets)
-    assert tsc.afs.keys() == vcfc.afs.keys() == {4}
-    np.testing.assert_allclose(tsc.afs[4], vcfc.afs[4])
+    np.testing.assert_allclose(tsc.hets[0], vcfc.hets[0])
+    np.testing.assert_allclose(tsc.hets[1], vcfc.hets[1])
+    assert tsc.afs.keys() == vcfc.afs.keys() == {(4,)}
+    np.testing.assert_allclose(tsc.afs[4,], vcfc.afs[4,])
 
 
 @pytest.mark.slow
 def test_ld(large_sim):
     buckets = np.arange(0.5, 10.5, 0.5)
     tsc = Contig.from_ts(
-        ts=large_sim, genetic_map=1e-8, nodes=[(2 * i, 2 * i + 1) for i in range(10)]
+        ts=large_sim,
+        genetic_map=1e-8,
+        nodes=[(2 * i, 2 * i + 1) for i in range(10)],
+        ld_buckets=buckets,
     )
     assert len(tsc.ld) == len(buckets) - 1
+
+
+def test_multipop_equal_ts_vcf(sim_twopop):
+    tsc = Contig.from_ts(ts=sim_twopop, nodes=[(0, 1), (2, 3), (4, 5)])
+    with tempfile.TemporaryDirectory() as d:
+        path = os.path.join(d, "tmp.vcf")
+        with open(path, "w") as f:
+            sim_twopop.write_vcf(
+                f, position_transform=lambda x: (1 + np.array(x)).astype(int)
+            )
+        # call bcftools index to create the index file
+        os.system(f"bgzip {path}")
+        path += ".gz"
+        os.system(f"bcftools index {path}")
+        vcfc = Contig.from_vcf(
+            vcf_path=path,
+            sample_ids=["tsk_0", "tsk_1", "tsk_2"],
+            sample_pops={"tsk_0": "A", "tsk_1": "B", "tsk_2": "B"},
+            contig="1",
+            interval=(1, 1_000_001),
+        )
+    np.testing.assert_allclose(tsc.hets[0], vcfc.hets[0])
+    assert tsc.afs.keys() == vcfc.afs.keys() == {(2, 4)}
+    # FIXME if the mutation rate is high these will be different because tskit counts
+    # sites that have experienced more than 1 mutation, whereas we toss them.
+    np.testing.assert_allclose(tsc.afs[2, 4], vcfc.afs[2, 4])
+    assert tsc.populations == vcfc.populations == tuple("AB")
