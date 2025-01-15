57090252b0b25ad41177d06b44b2827960e8fd0d
Jonathan Terhorst <terhorst@gmail.com>
2025-01-15 15:28:22 -0500

    cache more things

--- src/phlash/__init__.py
+++ src/phlash/__init__.py
@@ -18,6 +18,12 @@ import jax
 
 jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_compilation_cache_dir", platformdirs.user_cache_dir("phlash"))
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update(
+    "jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir"
+)
+
 
 import sys
 
